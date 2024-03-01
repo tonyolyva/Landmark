e6de12eb689e3fdf49949e9c3a924a2aefb9163b
tonyolyva <olyvatony@gmail.com>
2024-02-29 17:57:11 -0800

    Initial Commit

---
+++ Landmark/LandmarkApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarkApp.swift
+//  Landmark
+//
+//  Created by Anatoliy Olyva on 2/29/24.
+//
+
+import SwiftUI
+
+@main
+struct LandmarkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

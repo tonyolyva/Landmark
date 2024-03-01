d49d4bedbc0ca6afea88156463c5bd21700b34d3
tonyolyva <olyvatony@gmail.com>
2024-02-29 19:22:00 -0800

    image view - section 4: done

---
+++ Landmark/CircleImage.swift
@@ -0,0 +1,23 @@
+//
+//  CircleImage.swift
+//  Landmark
+//
+//  Created by Anatoliy Olyva on 2/29/24.
+//
+
+import SwiftUI
+
+struct CircleImage: View {
+    var body: some View {
+        Image("turtlerock")
+            .clipShape(Circle())
+            .overlay {
+                Circle().stroke(.white, lineWidth: 4)
+            }
+            .shadow(radius: 7)
+    }
+}
+
+#Preview {
+    CircleImage()
+}
--- Landmark/ContentView.swift
+++ Landmark/ContentView.swift
@@ -10,14 +10,20 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("Turtle Rock")
+                .font(.title)
+            
+            HStack {
+                Text("Joshua Tree National Park")
+                    .font(.subheadline)
+                Spacer()
+                Text("California")
+                    .font(.subheadline)
+            }
         }
         .padding()
+        }
     }
-}
 
 #Preview {
     ContentView()

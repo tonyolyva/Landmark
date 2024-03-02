0b20514c48d6ef117bb813a5ebe0bb85c9513eda
tonyolyva <olyvatony@gmail.com>
2024-03-01 16:06:29 -0800

    added MapKit for Map view

---
+++ Landmark/MapView.swift
@@ -0,0 +1,26 @@
+//
+//  MapView.swift
+//  Landmark
+//
+//  Created by Anatoliy Olyva on 3/1/24.
+//
+
+import SwiftUI
+import MapKit
+
+struct MapView: View {
+    var body: some View {
+        Map(initialPosition: .region(region))
+    }
+    
+    private var region: MKCoordinateRegion {
+        MKCoordinateRegion(
+            center: CLLocationCoordinate2D(latitude: 34.011_286, longitude: -116.166_868),
+            span: MKCoordinateSpan(latitudeDelta: 0.2, longitudeDelta: 0.2)
+        )
+    }
+}
+
+#Preview {
+    MapView()
+}

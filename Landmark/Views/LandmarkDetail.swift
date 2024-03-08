4e517f159c5fc0eb1187df594f0216b98dc68048
tonyolyva <olyvatony@gmail.com>
2024-03-07 16:25:10 -0800

    done with section 7:  Pass data into child view

--- Landmark/Views/LandmarkDetail.swift
+++ Landmark/Views/LandmarkDetail.swift
@@ -8,41 +8,47 @@
 import SwiftUI
 
 struct LandmarkDetail: View {
+    var landmark: Landmark
+
     var body: some View {
-        VStack {
-            MapView()
+        ScrollView {
+            MapView(coordinate: landmark.locationCoordinate)
                 .frame(height: 300)
             
-            CircleImage()
+            CircleImage(image: landmark.image)
                 .offset(y: -130)
                 .padding(.bottom, -130)
             
-            VStack {
-                Text("Turtle Rock")
+            VStack(alignment: .leading) {
+                Text(landmark.name)
                     .font(.title)
                 
                 HStack {
-                    Text("Joshua Tree National Park")
-                        .font(.subheadline)
+                    Text(landmark.park)
                     Spacer()
-                    Text("California")
+                    Text(landmark.state)
                 }
                 .font(.subheadline)
                 .foregroundStyle(.secondary)
 
                 Divider()
-                
-                Text("About Turtle Rock")
+
+                Text("About \(landmark.name)")
                     .font(.title2)
-                Text("Descriptive text goes here.")
+                Text(landmark.description)
             }
             .padding()
-            
-            Spacer()
+
         }
-    }    
+
+    }
+
 }
 
+
+
 #Preview {
-    LandmarkDetail()
+
+    LandmarkDetail(landmark: landmarks[0])
+
 }
--- Landmark/Views/LandmarkList.swift
+++ Landmark/Views/LandmarkList.swift
@@ -8,12 +8,11 @@
 import SwiftUI
 
 struct LandmarkList: View {
-
     var body: some View {
         NavigationSplitView {
             List(landmarks) { landmark in
                 NavigationLink {
-                    LandmarkDetail()
+                    LandmarkDetail(landmark: landmark)
 
                 } label: {
                     LandmarkRow(landmark: landmark)
